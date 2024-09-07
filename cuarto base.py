823a493a0ebe8d8dd1734d986898be322d346d72
javier12s <151806272+javier12s@users.noreply.github.com>
2024-09-07 16:53:03 -0600

    Add files via upload

---
+++ cuarto base.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+
+diccionario_python=¨{
+    "analisis":10,
+    "calificacion":20,
+    "Matematicas":30,
+    "derecho":40,
+    "español":50,
+    }
+edades_diccionario=pd.series(dicionario_python)
